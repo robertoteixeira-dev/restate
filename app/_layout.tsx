051adabcbf0a2150092f1ba3a49600ad9b6f2fc8
dev@thevoiceactorstudio.com <dev@thevoiceactorstudio.com>
2025-01-05 11:14:32 -0300

    feat: login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
 import "./global.css";
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  
+
   const [fontsLoaded] = useFonts({
-    'Rubik-Bold': require('@/assets/fonts/Rubik-Bold.ttf'),
-    'Rubik-ExtraBold': require('@/assets/fonts/Rubik-ExtraBold.ttf'),
-    'Rubik-Light': require('@/assets/fonts/Rubik-Light.ttf'),
-    'Rubik-Medium': require('@/assets/fonts/Rubik-Medium.ttf'),
-    'Rubik-Regular': require('@/assets/fonts/Rubik-Regular.ttf'),
-    'Rubik-SemiBold': require('@/assets/fonts/Rubik-SemiBold.ttf'),
+    "Rubik-Bold": require("@/assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("@/assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("@/assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("@/assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("@/assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("@/assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -34,13 +39,15 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="sign-in" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="sign-in" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
